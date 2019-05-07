5fd4c7b54f602ae7670b912d56f8427e1cb4afd4
Scott Weinberg <sdw328@nyu.edu>
2019-05-07 02:21:25 -0400

    update

--- js/3.js
+++ js/3.js
@@ -1,2 +1,17 @@
-var images = document.getElementsByClassName;
+var images = document.getElementsByClassName("image");
+var button= document.getElementById("randomize");
+// var randomnum=Math.floor(Math.random() * 35)+1;
+
+function random(){
+	for (i=0;i<images.length;i++){
+		var randomnum=Math.floor(Math.random() * 35)+1;
+		images[i].src = "images/"+randomnum+".jpg";
+	}
+}
+
+button.addEventListener("click", random);
+
+
+
+
 
